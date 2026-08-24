b0025de50b5e1a4f1cd26cf2b6a34bf48a6b755f
weicj <weicj520@gmail.com>
2026-08-24 17:41:14 +0800

    test(tool-parser): cover multiple Qwen function names

--- tests/tool_parsers/test_qwen3xml_streaming.py
+++ tests/tool_parsers/test_qwen3xml_streaming.py
@@ -37,3 +37,43 @@ def test_function_name_is_only_emitted_in_first_delta():
         for call in calls[1:]
         if call.function is not None
     )
+
+
+def test_function_name_is_emitted_once_for_each_tool_call():
+    parser = StreamingXMLToolCallParser()
+    deltas = _deltas(
+        parser,
+        [
+            "<tool_call>",
+            "<function=search_weather>",
+            "<parameter=city>Shanghai</parameter>",
+            "</function>",
+            "</tool_call>",
+            "<tool_call>",
+            "<function=get_time>",
+            "<parameter=timezone>UTC</parameter>",
+            "</function>",
+            "</tool_call>",
+        ],
+    )
+
+    calls_by_index = {}
+    for call in _tool_calls(deltas):
+        calls_by_index.setdefault(call.index, []).append(call)
+
+    assert set(calls_by_index) == {0, 1}
+    for expected_name, calls in zip(
+        ("search_weather", "get_time"),
+        (calls_by_index[0], calls_by_index[1]),
+        strict=True,
+    ):
+        assert calls[0].function is not None
+        assert calls[0].function.name == expected_name
+        assert all(
+            call.function is None or call.function.name is None for call in calls[1:]
+        )
+        assert all(
+            "name" not in call.function.model_dump(exclude_unset=True)
+            for call in calls[1:]
+            if call.function is not None
+        )
